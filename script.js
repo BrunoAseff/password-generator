c3f7ccaf3aac79f2a478b1f6bf1a46fcb8a4d655
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-05-01 14:03:07 +0000

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-const symbols = "!@#$%&*()-_+=[]{}|;:<>/?";
-const numbers = "0123456789";
-const characters = upperLetters + lowerLetters + symbols + numbers;
-const passwordLength = 10;
+const sliderEl = document.querySelector("#range");
+const sliderValue = document.querySelector(".value");
+
+sliderEl.addEventListener("input", (event) => {
+  const tempSliderValue = event.target.value;
+
+  sliderValue.textContent = tempSliderValue;
+
+  const progress = (tempSliderValue / sliderEl.max) * 100;
+
+  sliderEl.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
+});
 
 function copyContent() {
   const result = document.getElementById("result");
@@ -20,14 +26,29 @@ function copyContent() {
 }
 
 function generatePassword() {
+  let passwordLength = document.getElementById("range").value;
+  console.log(passwordLength);
+
+  const options = {
+    numbers: "0123456789",
+    lower: "abcdefghijklmnopqrstuvwxyz",
+    upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    symbols: "!@#$%&*()-_+=[]{}|;:<>/?",
+  };
+
+  let characters = "";
+  for (const option in options) {
+    if (document.getElementById(option).checked) {
+      characters += options[option];
+    }
+  }
+
   let password = "";
   for (let i = passwordLength; i > 0; i--) {
     const characterIndex = Math.floor(Math.random() * characters.length);
-
     password += characters[characterIndex];
-
-    const result = document.getElementById("result");
-
-    result.innerHTML = password;
   }
+
+  const result = document.getElementById("result");
+  result.innerHTML = password;
 }
